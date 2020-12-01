a35ce48b17fab4a5e14d3fcae8ba7a273002bbbf
Karol011 <karollacek1@gmail.com>
2020-12-01 12:03:32 +0100

    adding new http dependencies

--- src/main/java/io/github/mat3e/HelloServlet.java
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -1,6 +1,21 @@
 package io.github.mat3e;
 
-import javax.servlet.http.HttpServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Hello", urlPatterns = {"/api/*"})
 public class HelloServlet extends HttpServlet {
+    private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Request got");
+        resp.getWriter().write("Hello world");
+    }
 }
