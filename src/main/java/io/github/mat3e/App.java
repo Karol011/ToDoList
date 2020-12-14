bc6f61cd89a387d7d593e32c586d79297084e634
Karol011 <karollacek1@gmail.com>
2020-12-14 20:34:33 +0100

    connected jetty with hibernate'

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -4,6 +4,8 @@ import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
 import org.eclipse.jetty.plus.webapp.PlusConfiguration;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.component.AbstractLifeCycle;
+import org.eclipse.jetty.util.component.LifeCycle;
 import org.eclipse.jetty.webapp.*;
 
 
@@ -27,7 +29,12 @@ public class App {
 
         var server = new Server(8080);
         server.setHandler(webapp);
-
+        server.addLifeCycleListener(new AbstractLifeCycle.AbstractLifeCycleListener() {
+            @Override
+            public void lifeCycleStopped(final LifeCycle event) {
+                HibernateUtil.close();
+            }
+        });
         server.start();
         server.join();
     }
