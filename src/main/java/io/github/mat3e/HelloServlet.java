1d8dcfea6d28598c82c8de9b46ca7a6b8c359907
Karol011 <karollacek1@gmail.com>
2021-01-07 11:39:13 +0100

    -App now shows flags with languages to choose from, -minor bugfixes

--- src/main/java/io/github/mat3e/HelloServlet.java
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Hello", urlPatterns = {"/api/*"})
+@WebServlet(name = "Hello", urlPatterns = {"/api"})
 public class HelloServlet extends HttpServlet {
     private static final String NAME_PARAM = "name";
     private static final String LANG_PARAM = "lang";
--- src/main/java/io/github/mat3e/LangRepository.java
+++ src/main/java/io/github/mat3e/LangRepository.java
@@ -2,10 +2,21 @@ package io.github.mat3e;
 
 
 import lombok.Getter;
+
+import java.util.List;
 import java.util.Optional;
 
 @Getter
 public class LangRepository {
+
+    List<Lang> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        var result = session.createQuery("FROM Lang", Lang.class).list();
+        transaction.commit();
+        session.close();
+        return result;
+    }
 
     Optional<Lang> findById(Integer id) {
         var session = HibernateUtil.getSessionFactory().openSession();
